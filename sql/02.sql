c2543421534cc292afcdd1b6683600b433263034
Karl Puiras <karl@vianet.ca>
2023-12-27 15:22:51 -0500

    update database files

--- sql/02.sql
+++ sql/02.sql
@@ -3,16 +3,17 @@ ALTER TABLE `stripe_users`
 	ADD COLUMN `terms_reviewed` VARCHAR(255) NULL DEFAULT 'false' AFTER `customer_type`,
 	ADD COLUMN `zones_reviewed` VARCHAR(255) NULL DEFAULT 'false' AFTER `terms_reviewed`,
 	CHANGE COLUMN `temp_plan_expiration` `expiration` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci' AFTER `price_id`,
-	ADD COLUMN `total_spend` FLOAT NULL DEFAULT NULL AFTER `charges`,
-	ADD COLUMN `total_votes` INT(10) NULL DEFAULT NULL AFTER `total_spend`,
+	CHANGE COLUMN `charges` `charges` INT(10) NOT NULL DEFAULT '0' AFTER `charge_id`;
+	ADD COLUMN `total_spend` FLOAT NOT NULL DEFAULT '0' AFTER `charges`,
+	ADD COLUMN `total_votes` INT(10) NOT NULL DEFAULT '1' AFTER `total_spend`,
 	ADD COLUMN `zone_votes` JSON NULL DEFAULT NULL AFTER `total_votes`;
 
 CREATE TABLE `service_zones` (
 	`zone_name` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
 	`parent_zone` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-	`total_votes` INT(10) NOT NULL DEFAULT NULL,
-	`calc_workers` INT(10) NOT NULL DEFAULT NULL,
-	`admin_worker_override` INT(10) NOT NULL DEFAULT NULL,
-	`assigned_workers` INT(10) NOT NULL DEFAULT NULL,
+	`total_votes` INT(10) NOT NULL DEFAULT '0',
+	`calc_workers` INT(10) NOT NULL DEFAULT '0',
+	`admin_worker_override` INT(10) NOT NULL DEFAULT '0',
+	`assigned_workers` INT(10) NOT NULL DEFAULT '0',
 	PRIMARY KEY (`zone_name`) USING BTREE
 );
